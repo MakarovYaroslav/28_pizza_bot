33bebdf6154680c25d527913bdddf02e7a4d0721
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-01-31 13:24:48 +0300

    Добавлена таблица связей

--- models.py
+++ models.py
@@ -1,19 +1,23 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey, Text
+from sqlalchemy import Column, String, Integer, Text, ForeignKey, Table
 from sqlalchemy.orm import relationship
-
 
 Base = declarative_base()
 
+association_table = Table('pizza_choice', Base.metadata,
+                          Column('pizza_id', Integer, ForeignKey('pizza.identifier')),
+                          Column('choice_id', Integer, ForeignKey('choice.identifier'))
+                          )
+
 
 class Pizza(Base):
-    __tablename__ = 'pizza'
+    __tablename__ = "pizza"
     identifier = Column(Integer, primary_key=True)
     title = Column(String(100))
     description = Column(Text)
     choices = relationship(
         'Choice',
-        secondary='pizza_link'
+        secondary=association_table
     )
 
     def __str__(self):
@@ -21,20 +25,10 @@ class Pizza(Base):
 
 
 class Choice(Base):
-    __tablename__ = 'choice'
+    __tablename__ = "choice"
     identifier = Column(Integer, primary_key=True)
     title = Column(String(100))
     price = Column(Integer)
 
     def __str__(self):
         return "%s - %sруб." % (self.title, self.price)
-
-
-class PizzaChoiceLink(Base):
-    __tablename__ = 'pizza_link'
-    pizza_id = Column(Integer,
-                      ForeignKey('pizza.identifier'),
-                      primary_key=True)
-    choice_id = Column(Integer,
-                       ForeignKey('choice.identifier'),
-                       primary_key=True)
